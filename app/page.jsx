1bd24a241a7d483bdd88a2fc25fad69cc8a3ebcd
06yasue <101648951+06yasue@users.noreply.github.com>
2026-05-22 16:09:48 +0700

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="container text-center" style={{ marginTop: '150px' }}>
+      <div className="jumbotron" style={{ backgroundColor: 'transparent' }}>
+        <h1>Coming Soon</h1>
+        <p className="lead">Halaman Utama sedang dalam tahap pengembangan.</p>
+      </div>
+    </div>
+  );
+}
