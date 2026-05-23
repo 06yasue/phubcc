fb233925ac44bf36b05122d4ddc57f2c721dc583
06yasue <101648951+06yasue@users.noreply.github.com>
2026-05-23 10:45:50 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const siteConfig = {
-  sitename: "VideoApp",
-  domain: "phubcc.vercel.app" // Ubah dengan domain Vercel asli kamu
+  sitename: "StreamHub",
+  domain: "movies-next45.eu.org" // Ubah dengan domain Vercel asli kamu
 };
 
 export default siteConfig;
