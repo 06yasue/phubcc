4fd12953047cd8e31094603466acd68bec133721
06yasue <101648951+06yasue@users.noreply.github.com>
2026-05-23 19:00:30 +0700

    Create robots.js

---
+++ app/robots.js
@@ -0,0 +1,14 @@
+import siteConfig from '@/config';
+
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      // Blokir bot Google biar gak ngintip halaman admin/rahasia lo
+      disallow: ['/login', '/settings', '/list', '/upload'],
+    },
+    // Kasih tau Google posisi Sitemap Index lo
+    sitemap: `https://${siteConfig.domain}/sitemap.xml`,
+  }
+}
