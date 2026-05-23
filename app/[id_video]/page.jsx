bba10ee7b393bd6831b29008ca0160ac7c970ae7
06yasue <101648951+06yasue@users.noreply.github.com>
2026-05-23 13:24:48 +0700

    Update page.jsx

--- app/[id_video]/page.jsx
+++ app/[id_video]/page.jsx
@@ -100,16 +100,14 @@ export default async function FakeVideoPage({ params }) {
           </div>
         </div>
        
-        <div className="top-ads-wrapper">
+ <div className="top-ads-wrapper">
 
-  {/* MOBILE */}
   {siteSettings?.ads_mobile && (
     <div className="adsterra-mobile">
       <AdsBlock html={siteSettings.ads_mobile} />
     </div>
   )}
 
-  {/* DESKTOP */}
   {siteSettings?.ads_desktop && (
     <div className="adsterra-desktop">
       <AdsBlock html={siteSettings.ads_desktop} />
