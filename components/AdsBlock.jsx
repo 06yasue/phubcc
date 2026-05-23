116c875b44f689164b0482586f76b0748c0cd5ae
06yasue <101648951+06yasue@users.noreply.github.com>
2026-05-23 14:13:48 +0700

    Update AdsBlock.jsx

--- components/AdsBlock.jsx
+++ components/AdsBlock.jsx
@@ -10,23 +10,19 @@ export default function AdsBlock({ html }) {
 
     if (!ref.current) return;
 
-    // inject html
     ref.current.innerHTML = html;
 
-    // rerun scripts
     const scripts = ref.current.querySelectorAll('script');
 
     scripts.forEach((script) => {
 
       const newScript = document.createElement('script');
 
-      // copy src
       if (script.src) {
         newScript.src = script.src;
         newScript.async = true;
       }
 
-      // copy inline js
       newScript.innerHTML = script.innerHTML;
 
       script.parentNode.replaceChild(newScript, script);
