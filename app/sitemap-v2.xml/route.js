32661d41ceab57097c71fd8ae7a9479da4e71f31
06yasue <101648951+06yasue@users.noreply.github.com>
2026-05-23 21:40:50 +0700

    Update route.js

--- app/sitemap-v2.xml/route.js
+++ app/sitemap-v2.xml/route.js
@@ -5,15 +5,14 @@ export const dynamic = 'force-dynamic';
 
 export async function GET() {
   try {
-    // Gw hapus pemanggilan kolom 'slug' karena takutnya bikin error
-    const res = await turso.execute('SELECT id FROM urls ORDER BY id DESC LIMIT 5000');
+    const res = await turso.execute('SELECT id_video FROM video_txt ORDER BY id_video DESC LIMIT 5000');
     
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${res.rows.map(row => {
           return `
             <url>
-              <loc>https://${siteConfig.domain}/tube_${row.id}/play</loc>
+              <loc>https://${siteConfig.domain}/tube_${row.id_video}/play</loc>
               <changefreq>daily</changefreq>
               <priority>0.9</priority>
             </url>
@@ -25,7 +24,6 @@ export async function GET() {
     return new Response(xml, { headers: { 'Content-Type': 'text/xml' } });
 
   } catch (error) {
-    // KALAU ERROR, MUNCULIN PESANNYA DI LAYAR BIAR KETAHUAN!
     return new Response('BOS ADA ERROR DI DATABASE: ' + error.message, { status: 500 });
   }
 }
